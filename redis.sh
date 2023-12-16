18d353a9eb84c6ae751732b4a439b5a7e1d8cffc
Satish-Kalari <sarvag@gmail.com>
2023-12-16 10:13:56 +0530

    adding netstat command to monodb.sh file

--- redis.sh
+++ redis.sh
@@ -40,7 +40,7 @@ VALIDATE $? "Enabling redis 6.2 package"
 dnf install redis -y &>> $LOGFILE
 VALIDATE $? "Intalling redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $LOGFILE
 VALIDATE $? "Remote acess for redis"
 
 systemctl start redis &>> $LOGFILE
