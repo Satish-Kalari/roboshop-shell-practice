b7053d6edf5e015b3b3115834bc713889a18d36d
Satish-Kalari <sarvag@gmail.com>
2023-12-20 13:33:46 +0530

    robo auto ec

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -8,43 +8,50 @@ DOMAIN_NAME="projoy.store"
 
 for i in "${INSTANCES[@]}"
 do
-  # Check if instance exists
-  INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$i" --query 'Reservations[].Instances[].InstanceId' --output text)
-  if [ -z "$INSTANCE_ID" ]
-  then
-      # Instance does not exist, create it
-      if [ $i == "monodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-      then
-          INSTANCES_TYPE="t3.small"
-      else
-          INSTANCES_TYPE="t2.micro"
-      fi
+ # Check if instance exists
+ INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$i" --query 'Reservations[].Instances[].InstanceId' --output text)
+ if [ -z "$INSTANCE_ID" ]
+ then
+     # Instance does not exist, create it
+     if [ $i == "monodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+     then
+         INSTANCES_TYPE="t3.small"
+     else
+         INSTANCES_TYPE="t2.micro"
+     fi
 
-      IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+     if [ $i == "web" ]
+     then
+         # For "web" instance, get public IP
+         IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
+     else
+         # For all other instances, get private IP
+         IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+     fi
 
-      echo "$i: $IP_ADDRESS"
+     echo "$i: $IP_ADDRESS"
 
-      #create R53 record, make sure you delete existing record
-      aws route53 change-resource-record-sets \
-      --hosted-zone-id $ZONE_ID \
-      --change-batch '
-      {
-          "Comment": "Creating a record set for cognito endpoint"
-          ,"Changes": [{
-          "Action"             : "UPSERT" 
-          ,"ResourceRecordSet" : {
-              "Name"             : "'$i'.'$DOMAIN_NAME'"
-              ,"Type"            : "A"
-              ,"TTL"             : 1
-              ,"ResourceRecords" : [{
-                 "Value"        : "'$IP_ADDRESS'"
-              }]
-          }
-          }]
-      }
-          '
-  else
-      # Instance exists, skip creation
-      echo "$i instance already exists, skipping creation."
-  fi
+     #create R53 record, make sure you delete existing record
+     aws route53 change-resource-record-sets \
+     --hosted-zone-id $ZONE_ID \
+     --change-batch '
+     {
+         "Comment": "Creating a record set for cognito endpoint"
+         ,"Changes": [{
+         "Action"            : "UPSERT" 
+         ,"ResourceRecordSet" : {
+             "Name"            : "'$i'.'$DOMAIN_NAME'"
+             ,"Type"           : "A"
+             ,"TTL"            : 1
+             ,"ResourceRecords" : [{
+                "Value"       : "'$IP_ADDRESS'"
+             }]
+         }
+         }]
+     }
+         '
+ else
+     # Instance exists, skip creation
+     echo "$i instance already exists, skipping creation."
+ fi
 done
